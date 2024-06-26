0c06445550efce4824be4293a12bef88282a6430
Ikechukwu Nwakanma <ike.nwakanma@internetbrands.com>
2024-06-26 13:40:40 +0100

    Init dotfiles

---
+++ nvim/lua/user/plugins/blankline.lua
@@ -0,0 +1,18 @@
+require('ibl').setup({ 
+    exclude = {
+      filetypes = {
+        'help',
+        'terminal',
+        'dashboard',
+        'packer',
+        'lspinfo',
+        'TelescopePrompt',
+        'TelescopeResults',
+      }, 
+      buftypes = {
+          'terminal',
+          'NvimTree',
+        }
+    }
+})
+
